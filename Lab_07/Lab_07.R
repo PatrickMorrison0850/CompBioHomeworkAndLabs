322cf47103ae5112fe525e750c3d0724a95a8aca
Patrick Morrison <pamo0850@colorado.edu>
2017-03-17 09:55:33 -0500

    Lab 08 Script

--- Lab_07/Lab_07.R
+++ Lab_07/Lab_07.R
@@ -16,20 +16,36 @@ FiboFunct(1,2)
 #Question 2
 ExpGrowthFunct <- function(I = 2, G = 2, r = 1, K = 1){
   n <- rep(I,G)
-  for(t in (2,Gen_Time)){
+  for(t in (2, Gen_Time)){
     n[t] <- n[t-1] + ( r * n[t-1] * (K - n[t-1])/K)
   }
   return(n)
 }
 #Question 3
-
-
-
-
-
-
-
-
+Vect <- read.csv("Vector1.csv")
+LargeAdjacencyMatrix <- read.csv("LargeAdjacencyMatrix.csv")
+a <- length(which(LargeAdjacencyMatrix == 1))
+Column <- rep(0, a)
+Row <- rep(0,a)
+Value1 <- rep(1,a)
+Value0 <- rep(0,a)
+Adjacency1 <- data.frame(Column,Row,Value)
+for(i in seq(1,a)){
+  for(o in seq(1,a)){
+    if(LargeAdjacencyMatrix[o,i] = 1){
+      Adjacency1$Column <- o
+      Adjacency1$Row <- i
+    }
+  }
+}
+for(y in seq(1,a)){
+  for(u in seq(1,a)){
+    if(LargeAdjacencyMatrix[u,y] = 0){
+      Adjacency0$Column <- u
+      Adjacency0$Row <- y
+    }
+  }
+}
 
 
 
